967c070792b07507db763cafd5064ccdfca04954
Stanislav_Velikov <s.velikov@astoundcommerce.com>
2023-04-30 16:45:41 +0300

    1:46:00

--- app.js
+++ app.js
@@ -2,6 +2,78 @@ const fs = require('fs/promises');
 
 // Watching command file
 (async () => {
+    // commands
+    const CREATE_FILE = 'create a file';
+    const DELETE_FILE = 'delete the file';
+    const RENAME_FILE = 'rename the file';
+    const ADD_TO_FILE = 'add to the file';
+
+    const createFile = async (path) => {
+        try {
+            // we want to check whether or not we have that file
+            const existingFileHandle = await fs.open(path, 'r');
+            existingFileHandle.close();
+            
+            // we already have that file...
+            return console.log(`The file ${path} already exists.`);
+        } catch (e) {
+            // we don't have the file, now we should create it
+            const newFileHandle = await fs.open(path, 'w');
+            console.log('A new file was successfully created.');
+            newFileHandle.close();
+        }
+    };
+    
+    const deleteFile = async (path) => {
+        console.log(`Deleting ${path}`);
+        try {
+            // check if the file for deleting exist
+            const existingFileHandle = await fs.open(path, 'r');
+            existingFileHandle.close();
+
+            await fs.rm(path);
+
+            return console.log(`The file ${path} has been successfully deleted.`)
+
+            // return console.log(`The file ${path} doesn\'t exist.`);
+        } catch (e) {
+            return console.log(`The file ${path} doesn\'t exist.`);
+        }
+    }
+
+
+    const renameFile = async (oldPath, newPath) => {
+        console.log(`Rename ${oldPath} to ${newPath}`);
+        try {
+            // check if the file for deleting exist
+            const existingFileHandle = await fs.open(oldPath, 'r');
+            existingFileHandle.close();
+
+            await fs.rename(oldPath, newPath);
+
+            return console.log(`The file ${path} has been successfully renamed to ${newPath}`);
+        } catch (e) {
+            return console.log(`The file you are trying to rename doesn\'t exist.`)
+        }
+    }
+
+
+    const addToFile = async (path, content) => {
+        console.log(`Adding to ${path}`);
+        console.log(`Content: ${content}`);
+        try {
+            // check if the file for deleting exist
+            const existingFileHandle = await fs.open(path, 'r');
+            existingFileHandle.close();
+            
+            await fs.writeFile(path, Buffer.from(content));
+
+            return console.log(`Content has been successfully added.`)
+        } catch (e) {
+            return console.log(`The file ${path} doesn\'t exist.`);
+        }
+    }
+
     const commandFileHandler = await fs.open('./command.txt', 'r');
     const watcher = fs.watch('./command.txt');
 
@@ -14,19 +86,61 @@ const fs = require('fs/promises');
         // the location at which we want to start filling our buffer
         const offset = 0;
         const length = buff.byteLength;
-        // the position that we want to start reading fht file from
+        // the position that we want to start reading the file from
         const position = 0;
         
         // we always want to read the whole content (from beginning all the way to the end)
-        const content = await commandFileHandler.read(
+        await commandFileHandler.read(
             buff,
             offset,
             length,
             position
         );
-        console.log(content)
+        
+        // decoder 0101 => meaningful
+        // encoder meaningful => 0101
+
+        const command = buff.toString('utf-8');
+
+        // create a file:
+        // create a file <path>
+        if (command.includes(CREATE_FILE)) {
+            const filePath = command.substring(CREATE_FILE.length + 1);
+            createFile(filePath);
+        }
+        
+        // delete a file
+        // delete the file <path>
+        if (command.includes(DELETE_FILE)) {
+            const filePath = command.substring(DELETE_FILE.length + 1);
+            deleteFile(filePath);
+        }
+
+
+        // rename file
+        // rename the file <path> to <new-path>
+        if (command.includes(RENAME_FILE)) {
+            const _idx = command.indexOf(' to ');
+            const oldFilePath = command.substring(RENAME_FILE.length + 1, _idx);
+            const newFilePath = command.substring(_idx + 4);
+
+            renameFile(oldFilePath, newFilePath);
+        }
+    
+        // add to file:
+        // add to the file <path> this content: <content>
+        if (command.includes(ADD_TO_FILE)) {
+            const _idx = command.indexOf(' this content: ');
+            const filePath = command.substring(ADD_TO_FILE.length  + 1, _idx);
+            const content = command.substring(_idx + 15);
+
+            console.log('-----', filePath)
+
+            addToFile(filePath, content);
+        }
     })
 
+    // watcher...
     for await (const event of watcher) {
         if (event.eventType === 'change') {
             commandFileHandler.emit('change');
